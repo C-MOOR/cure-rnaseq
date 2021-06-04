0deee41c98b7bbdbe669a8652624ed72d54bbefc
Frederick Tan <tan@carnegiescience.edu>
2021-06-04 09:58:05 -0400

    Add README.md to tutorials

---
+++ tutorials/README.md
@@ -0,0 +1,16 @@
+# C-MOOR CURE CCC FA21
+
+## Introduction to Course -- Due August 12, 2021
+
+No tutorials - first week of class. Welcome!
+
+## Genes and Biological Databases -- Due August 17, 2021
+
+- [Introduction to Model Organisms](tutorials/Model_Organisms_Intro/)
+- [Introduction to Biological Databases](tutorials/Biological_Databases_Intro/)
+- [Biological Databases: Human Protein Atlas](tutorials/Biological_Databases_HPA/)
+
+## Introduction to R -- Due August 19, 2021
+
+- [Meet R!](tutorials/Intro_R/)
+
---
+++ tutorials/make_readme.R
@@ -0,0 +1,66 @@
+TITLE = "C-MOOR CURE CCC FA21"
+YAML_FILE = "index.yaml"
+MD_FILE = "README.md"
+BASE_URL = "tutorials/"
+
+# read yaml
+y <- yaml::yaml.load_file(YAML_FILE)
+
+
+#### HELPER FUNCTIONS ####
+
+# extract tutorial metadata
+get_tutorial_metadata <- function(dir_name) {
+  f <- file.path(dir_name, paste0(dir_name, ".Rmd"))
+  title <- grep("^title", readLines(f), value=TRUE)[1]
+  title <- strsplit(title, split="\"")[[1]][2]
+}
+
+# get url for tutorial
+get_tuturial_url <- function(dir_name, base_url) {
+  paste0(base_url, dir_name, "/")
+}
+
+# build tutorial link tag
+make_tutorial_link_md <- function(dir_name, base_url) {
+  paste0(
+    "[", get_tutorial_metadata(dir_name), "]",
+    "(", get_tuturial_url(dir_name, base_url), ")"
+  )
+}
+
+# build html for a single day
+make_day_md <- function(day_info, base_url) {
+
+  # Day (h2)
+  date <- format(as.Date(day_info$due_date, "%m-%d-%Y"), "%B %d, %Y")
+  day <- paste0("## ", day_info$name, " -- Due ", date, "\n\n")
+  
+  # Message
+  if (!is.null(day_info$message)) {
+    if (!(day_info$message=="")) {
+      m <- paste0(day_info$message, "\n\n")
+      day <- paste0(day, m)
+    }
+  }
+
+  # Tutorials
+  if (!is.null(day_info$tutorial_dir)) {
+    ol <- lapply(day_info$tutorial_dir, make_tutorial_link_md, base_url)
+    ol <- paste0("- ", ol, "\n", collapse="")
+    day <- paste0(day, ol, "\n")
+  }
+  day
+}
+
+#### BUILD MARKDOWN ####
+
+# add title
+title <- c("# ", TITLE, "\n\n")
+
+# add assignments
+tutorials <- lapply(y, make_day_md, BASE_URL)
+tutorials <- paste0(tutorials)
+
+# write to file
+cat(title, tutorials, file=MD_FILE, sep="")
