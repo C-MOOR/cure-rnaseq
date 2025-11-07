ce795ce0dd3ec7dd2961aae69f9037f74023ec0a
sayumiyork <syork@ndm.edu>
2025-11-06 22:04:39 -0500

    Add AnVIL Startup Script
    * Create anvil folder in cure-rnaseq
    * Add AnVIL startup script to the anvil folder

---
+++ anvil/C-MOOR_Startup_Script.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+    cd /home/rstudio
+    chown -R 1000 /home/rstudio
+    mkdir rnaseq
+    mkdir 16s
+    chown -R 1000 /home/rstudio/16s
+    chown -R 1000 /home/rstudio/rnaseq
+
+    R -e "install.packages('learnr')"
+    R -e "BiocManager::install('clusterProfiler')"
+    R -e "BiocManager::install('org.Dm.eg.db')"
+    R -e "BiocManager::install('org.Mm.eg.db')"
+    R -e "BiocManager::install('phyloseq')"
+    R -e "BiocManager::install('DOSE')" 
+    R -e "remotes::install_github('C-MOOR/testdriveRData')"
+    R -e "remotes::install_github('C-MOOR/MarianesMidgutData')"
+    R -e "remotes::install_github('C-MOOR/GuthrieMisoData')"
+    R -e "remotes::install_github('C-MOOR/SharonBrainData')"
+    R -e "remotes::install_github('C-MOOR/HumanGutMicrobiomeData')"
+    
+    git clone -b meet-sharongutbrain https://github.com/c-moor/cure-rnaseq/ /home/rstudio/cure-rnaseq
+    chown -R 1000 /home/rstudio/cure-rnaseq
+    mv cure-rnaseq cmoor-files
+
+    cd /home/rstudio/rnaseq
+
+    ln -s /home/rstudio/cmoor-files/tutorials/Model_Organisms_Intro /home/rstudio/rnaseq/1-intro-model-org
+    ln -s /home/rstudio/cmoor-files/tutorials/Model_Organisms_Drosophila /home/rstudio/rnaseq/2-model-org-dmel
+    ln -s /home/rstudio/cmoor-files/tutorials/Biological_Databases_Intro /home/rstudio/rnaseq/3-intro-db
+    ln -s /home/rstudio/cmoor-files/tutorials/Biological_Databases_FlyBase /home/rstudio/rnaseq/4-db-flybase
+    ln -s /home/rstudio/cmoor-files/tutorials/Biological_Databases_HPA /home/rstudio/rnaseq/5-db-hpa
+    ln -s /home/rstudio/cmoor-files/tutorials/Biotechnology_Next-Gen_Sequencing /home/rstudio/rnaseq/6-biotech-ngs
+    ln -s /home/rstudio/cmoor-files/tutorials/Biotechnology_RNA-seq /home/rstudio/rnaseq/7-biotech-rnaseq
+    ln -s /home/rstudio/cmoor-files/tutorials/Clovis_RNAseq_single_sample /home/rstudio/rnaseq/8-intro-rnaseq
+    ln -s /home/rstudio/cmoor-files/tutorials/Differential_Expression /home/rstudio/rnaseq/9-diff-exp
+    ln -s /home/rstudio/cmoor-files/tutorials/rna-seq-project-work /home/rstudio/rnaseq/10-rnaseq-proj
+
+    cd /home/rstudio/16s
+    
+    ln -s /home/rstudio/cmoor-files/tutorials/explore-phyloseq/ /home/rstudio/16s/1-explore-ps
+    ln -s /home/rstudio/cmoor-files/tutorials/analyze-phyloseq/ /home/rstudio/16s/2-analyze-ps
+    ln -s /home/rstudio/cmoor-files/tutorials/16S-amplicon-project-work/ /home/rstudio/16s/3-16s-project
+
+   
+
+
